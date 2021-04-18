b24945b57fbfb97d94674ebea6e38b72d129da96
notGonim <mhmodgonim96@gmail.com>
2021-04-18 09:35:09 +0200

    Implementing in-browser  bundling  62

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,49 @@
+import * as esbuild from 'esbuild-wasm'
+import { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
 const App =()=>{
-    return <h1>working fine </h1>
+    
+    const [input ,setInput]=useState('')
+const [code,setCode]=useState('')
+
+const ref=useRef<any>()
+
+
+const startService =async()=>{
+ref.current = await  esbuild.initialize({
+    worker: true,
+    wasmURL: '/esbuild.wasm'
+  })
+ref.current=true
+}
+
+
+useEffect(()=>{
+startService()
+
+},[])
+
+const onSubmitHandler= async()=>{
+ 
+if(!ref.current)
+return
+const res=await   esbuild.transform(input,{
+            loader:'jsx',
+            target:'es2015'
+        }) 
+         setCode(res.code)
+       }
+
+
+return  (
+        <div className="">
+            <textarea value={input} onChange={(e)=>setInput(e.target.value)} ></textarea>
+<button onClick={onSubmitHandler}>Submit</button>
+<pre>{code}</pre>
+        </div>
+    )
 }
 
 
